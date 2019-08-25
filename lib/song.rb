351fd2ab07a5a317c7f2eb08969211838d3c8733
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-25 13:52:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -9,5 +9,10 @@ class Song
   def save
     self.class.all << self
   end
-
+  
 end
+
+song = Song.create
+Song.all.include?(song)
+
+song.name 
